7ca04228c8169609130086464339ab759fe619cb
Malith Sen <senanayake.malith@gmail.com>
2021-07-05 12:22:05 +0530

    Added Components to index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDom from 'react-dom'
 
 function BookList(){
-   return (
-   <section>
-      This is a booklist    
-   </section>
-   );
+    return(
+        <section>
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+        </section>
+    )
 }
 
+const Book = () => {
+    return( 
+    <article>
+       <Image />
+       <Title />
+       <Author />
+    </article>
+    )
+};
 
-ReactDOM.render(<BookList />, document.getElementById('root'));
+const Image = () => <img src="https://m.media-amazon.com/images/I/81eB+7+CkUL._AC_UY218_.jpg" alt="" />
+const Title = () => <h1>I love you back to the moon and more</h1>
+const Author = () => <h2>Amelia Hepworth</h2>
+ReactDom.render(<BookList />, document.getElementById('root'));
