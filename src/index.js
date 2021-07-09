d5ef75931d008fbe097ea22c320a7a856f58687f
Malith Sen <senanayake.malith@gmail.com>
2021-07-09 17:05:50 +0530

    Created a testing folder to test exporting and imporing modules. Edited books with more books

--- src/index.js
+++ src/index.js
@@ -4,24 +4,28 @@ import ReactDom from 'react-dom';
 //CSS
 import './index.css';
 
-//Creating object
-const books = [
-{
-    img : "https://m.media-amazon.com/images/I/81eB+7+CkUL._AC_UY218_.jpg",
-    title: 'I love you to the moon and back',
-    author: 'Amelia Hepworth'
-},
-{
-    img : "https://m.media-amazon.com/images/I/71aLultW5EL._AC_UY218_.jpg",
-    title: 'Our Class is a Family',
-    author: 'Shannon Oslen'
-}
-]
+import {books} from './books'
+import Book from './Book'
+import {greeting} from './testing/testing'
+
+
+ 
 
 function BookList(){
+    
     return(
+        
         <section className="booklist">
-           {books}
+            
+          {books.map((book) =>{
+            //   return <div>
+            //       <h3>{title}</h3>
+            //       <h6>{author}</h6>
+            //       <img src="{img}" alt="" />
+            //   </div>;
+            console.log(greeting);
+            return <Book key={book.id} {...book}></Book>;
+          })}
         </section>
     )
 }
@@ -29,17 +33,7 @@ function BookList(){
 // const Book = (props) => {  
     // const {img, title, author} = props
 //const Book = ({img,title, author, children}) => {  
-const Book = (props)=>{
-     const {img, title, author} = props;
-    return( 
-    <article className="book">
-       <img src={img} alt=""/>
-       <h1>{title}</h1>
-       <h4>{author}</h4>
-       
-    </article>
-    )
-};
+
 
 
 ReactDom.render(<BookList />, document.getElementById('root'));
